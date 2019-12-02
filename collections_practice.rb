6e34e7de0a94175ca0bf0fa2214b75145375a556
Tyrik Gibbs <tyrikgibbs@outlook.com>
2019-12-02 05:08:54 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -14,3 +14,31 @@ def sort_array_char_count(array)
     left.length <=> right.length
   end
 end
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  array
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  array.each do |item|
+    item[2] = "$"
+  end
+end
+
+def find_a(array)
+  array.find_all do |word|
+    word[0] == "a"
+  end
+
+def sum_array(array)
+  sum = 0
+  array.each do |num|
+    sum+=num
+  end
+  sum
+end 
